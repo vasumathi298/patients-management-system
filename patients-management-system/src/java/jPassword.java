0ec658fa235460cc178a1f3f66de43dd543eb0fb
vasumathi298 <72432533+vasumathi298@users.noreply.github.com>
2021-07-09 09:40:53 +0530

    Add files via upload

---
+++ patients-management-system/src/java/jPassword.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java;
+
+/**
+ *
+ * @author Vasumathi Narayanan
+ */
+public class jPassword {
+
+    public static void setText(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
